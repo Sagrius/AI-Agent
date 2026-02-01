c665731567aecb320018e80aa5476bfdebe96513
Sagrius <alexeibuic@gmail.com>
2026-02-01 18:17:03 +0200

    Imported TMP ,added tail slider as well for visual representation

--- Assets/Scripts/SetWingSlider.cs
+++ Assets/Scripts/SetWingSlider.cs
@@ -5,7 +5,7 @@ public class SetWingSlider : MonoBehaviour
 {
     public Slider L_WingSlider;
     public Slider R_WingSlider;
-
+    public Slider TailSlider;
     public void SetValueLeftWing(float newValue)
     {
         L_WingSlider.value = newValue;
@@ -13,5 +13,9 @@ public class SetWingSlider : MonoBehaviour
     public void SetValueRightWing(float newValue)
     {
         R_WingSlider.value = newValue;
+    }
+    public void SetValueTail(float newValue)
+    {
+        TailSlider.value = newValue;
     }
 }
